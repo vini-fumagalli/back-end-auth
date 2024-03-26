ee37210c8bd7d280e1049f3a102810d96229883f
fumas <vinifumagalli_@hotmail.com>
2024-03-26 14:26:23 -0300

    feat (AuthController): implementa método para gerar token de acesso e devolver ele juntamnete com informações e claims do usuário

--- src/Api.Application/Controllers/AuthController.cs
+++ src/Api.Application/Controllers/AuthController.cs
@@ -1,24 +1,33 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Api.CrossCutting.Configuration;
 using Api.Domain.Entities;
 using Api.Domain.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Application.Controllers;
 
 [ApiController]
-[Route("api")]
+[Route("api/auth")]
 public class AuthController : ControllerBase
 {
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppSettings _appSettings;
 
     public AuthController(
         SignInManager<IdentityUser> signInManager,
-        UserManager<IdentityUser> userManager
+        UserManager<IdentityUser> userManager,
+        IOptions<AppSettings> appSettings
     )
     {
         _signInManager = signInManager;
         _userManager = userManager;
+        _appSettings = appSettings.Value;
     }
 
     [HttpPost("sign-up")]
@@ -41,7 +50,7 @@ public class AuthController : ControllerBase
         {
             await _signInManager.SignInAsync(user, false);
             msg = "Usuário cadastrado com sucesso";
-            return Ok(new RespostaEntity(true, msg));
+            return Ok(new RespostaEntity(true, await GerarJwt(registerUser.Email), msg));
         }
 
         msg = "Falha ao tentar realizar cadastro";
@@ -60,7 +69,7 @@ public class AuthController : ControllerBase
         if (result.Succeeded)
         {
             msg = "Usuário logado com sucesso";
-            return Ok(new RespostaEntity(true, msg));
+            return Ok(new RespostaEntity(true, await GerarJwt(loginUser.Email), msg));
         }
 
         if (result.IsLockedOut)
@@ -73,8 +82,57 @@ public class AuthController : ControllerBase
         return BadRequest(new RespostaEntity(false, msg));
     }
 
-    // private async Task<string> GerarJwt(string email)
-    // {
-    //     return "To be continued...";
-    // }
+    private async Task<LoginResponseViewModel> GerarJwt(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        var claims = await _userManager.GetClaimsAsync(user!);
+        var userRoles = await _userManager.GetRolesAsync(user!);
+
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user!.Id));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Email, user!.Email!));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+        foreach (var userRole in userRoles)
+        {
+            claims.Add(new Claim("role", userRole));
+        }
+
+        var identityClaims = new ClaimsIdentity();
+        identityClaims.AddClaims(claims);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = _appSettings.Emissor,
+            Audience = _appSettings.ValidoEm,
+            Subject = identityClaims,
+            Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+        });
+
+        var encodedToken = tokenHandler.WriteToken(token);
+
+        return new LoginResponseViewModel
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+            UserToken = new UserTokenViewModel
+            {
+                Id = user.Id,
+                Email = user.Email!,
+                Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+            }
+        };
+    }
+
+    private static long ToUnixEpochDate(DateTime date)
+    {
+        return (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+    }
+
 }
