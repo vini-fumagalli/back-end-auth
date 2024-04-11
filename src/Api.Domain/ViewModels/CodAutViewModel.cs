2ca626dee1095fd7a99918e58467b02df1487ae6
fumas <vinifumagalli_@hotmail.com>
2024-04-11 13:56:11 -0300

    feat (CodAutViewModel): adiciona nova View Model

---
+++ src/Api.Domain/ViewModels/CodAutViewModel.cs
@@ -0,0 +1,8 @@
+namespace Api.Domain.ViewModels;
+
+public class CodAutViewModel
+{
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string CodAut { get; set; } = "";
+}
