909b325199c8a7e78f9ab23baa78dedb292381c1
Fumas <vinifumagalli_@hotmail.com>
2024-04-13 19:13:56 -0300

    feat (CreateClaimViewModel): implementa nova view model

---
+++ src/Api.Domain/ViewModels/CreateClaimViewModel.cs
@@ -0,0 +1,8 @@
+namespace Api.Domain.ViewModels;
+
+public class CreateClaimViewModel
+{
+    public string User { get; set; } = "";
+    public string Type { get; set; } = "";
+    public string Value { get; set; } = "";
+}
